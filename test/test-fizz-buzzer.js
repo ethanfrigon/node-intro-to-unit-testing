6911a770c217b23c3ea4ce2603216d44e0fb3b32
Ethan Frigon <ethan.frigon@gmail.com>
2018-11-16 08:49:25 -0800

    fizzbuzzer test complete

--- test/test-fizz-buzzer.js
+++ test/test-fizz-buzzer.js
@@ -2,15 +2,36 @@
 
 const expect = require('chai').expect;
 
-const fizzBuzzer = require('fizzBuzzer');
+const fizzBuzzer = require('../fizzBuzzer');
 
-describe('fizzbuzzer', function(){
+describe('fizzBuzzer', function(){
+
+  it('should return the input number for numbers not divisible by 3,5,15', function(){
+    const answer = fizzBuzzer(17);
+    expect(answer).to.equal(17);
+  });
 
   it('should return fizzbuzz for numbers divisible by 15', function(){
     const answer = fizzBuzzer(30);
-    expect(answer).to.equal('fizzbuzz');
-  } )
+    expect(answer).to.equal('fizz-buzz');
+  });
+  
+  it('should return fizz for numbers divisible by 3', function(){
+    const answer = fizzBuzzer(6);
+    expect(answer).to.equal('fizz');
+  });
 
+  it('should return buzz for numbers divisible by 5', function(){
+    const answer = fizzBuzzer(1055);
+    expect(answer).to.equal('buzz');
+  });
+  
+  it('should return an error if the input is not a number', function(){
+    // const answer = fizzBuzzer('foo');
+    expect(function(){
+      fizzBuzzer('foo');
+    }).to.throw(Error);
+  });
 
   
-})
+});
